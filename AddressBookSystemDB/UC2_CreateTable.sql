b9043275e362e6cef34a0039a3ad3a30e01ac567
Prasad <banprasad21@gmail.com>
2022-10-07 01:13:56 +0530

    [Raghav] Add: UC2 created table

---
+++ AddressBookSystemDB/UC2_CreateTable.sql
@@ -0,0 +1,18 @@
+------------UC1 Create Address Book Service DB------------
+create database AddressBookService
+-------Choose database-----------
+use AddressBookService
+---------Create Table ---------
+Create Table AddressBook
+(
+FirstName varchar(60) not null,
+LastName varchar(60) not null,
+Address varchar(150) not null,
+City varchar(60) not null,
+State varchar(60) not null,
+Zip  int not null,
+PhoneNumber bigint not null,
+Email varchar(100) not null
+);
+
+select * from AddressBook;
