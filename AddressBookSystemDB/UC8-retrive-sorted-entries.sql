2b1cc9d62d33d0cc3a5eaed663f94a3b43169b0d
Prasad <banprasad21@gmail.com>
2022-10-07 18:27:05 +0530

    [Raghav] Add: SQL query to retrive contacts in sorted manner i.e Orderby query

--- AddressBookSystemDB/UC8-retrive-sorted-entries.sql
+++ AddressBookSystemDB/UC8-retrive-sorted-entries.sql
@@ -17,14 +17,14 @@ Email varchar(100) not null
 
 -----UC3 Adding contacts in to table-------
 insert into AddressBook
-values('Raghav','Ban','Yavatmal','Yavatmal','Maharashtra',686574,11111111,'unthinkable@gmail.com'),
-('Ajay','Rathod','Ner','Yavatmal','Maharashtra',456321,7898789784,'AjR@gmail.co'),
-('Preeti','Sharma','Kanpur','kanpur','UttarPradesh',456321,7412589632,'psk@gmail.com');
+values('Prasad','Ban','32 street','Noida','Delhi',6345574,34134345111,'unique@gmail.com'),
+('Ajay','Rathod','57 ring road','Noida','Delhi',43434321,7823234584,'AjR@gmail.co');
+
 
 
 ----------UC 4 Update Contact--------------
 update AddressBook
-set city = 'Nagpur' where firstName='Raghav' and lastName='Ban';
+set city = 'Yavatmal' where firstName='Raghav' and lastName='Ban';
 
 ----------UC5 5 Delete Contact ------------
 delete from AddressBook
@@ -38,5 +38,10 @@ where city='Yavatmal';
 --------UC7 get size of address book -----------
 select count(*) as NoOfContacts from AddressBook
 where city='Yavatmal' and state='Maharashtra';
+
+-----------UC8 retrive sorted entries ----------
+select * from AddressBook
+where city = 'Noida'
+order by FirstName,LastName;
 
 select * from AddressBook;
